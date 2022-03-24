6117141bd6b28be3c8c92bca9c60f80d6f677078
Ricky Davenport <ricky.davenport@wwt.com>
2022-03-24 11:52:27 -0500

    configure type aliases

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,17 @@ const prod = process.env.NODE_ENV === 'production';
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
 	mode: prod ? 'production' : 'development',
 	entry: './src/index.tsx',
 	output: {
 		path: __dirname + '/build/'
+	},
+	resolve: {
+		plugins: [new TsConfigPathsPlugin()],
+		extensions: ['.ts', '.js']
 	},
 	module: {
 		rules: [
