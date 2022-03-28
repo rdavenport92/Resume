c91764406106f9ff513c260fc4af55388f6b0c04
Ricky Davenport <ricky.davenport@wwt.com>
2022-03-28 11:13:01 -0500

    add Resume

---
+++ src/views/Resume/Resume.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import Resume from './Resume';
+
+const mockBodyDataTestId = 'bodyDataTestId';
+const mockHeaderDataTestId = 'headerDataTestId';
+
+jest.mock('@layout', () => {
+	function MockHeader() {
+		return <div data-testid={mockHeaderDataTestId} />;
+	}
+
+	function MockBody() {
+		return <div data-testid={mockBodyDataTestId} />;
+	}
+
+	return {
+		Header: MockHeader,
+		Body: MockBody
+	};
+});
+
+describe('Resume', () => {
+	it('renders header', () => {
+		render(<Resume />);
+
+		const resumeWrapper = document.querySelector('.resume');
+		const headerWrapper = screen.queryByTestId(mockHeaderDataTestId);
+
+		expect(resumeWrapper?.children[0]).toBe(headerWrapper);
+	});
+
+	it('renders body', () => {
+		render(<Resume />);
+
+		const resumeWrapper = document.querySelector('.resume');
+		const bodyWrapper = screen.queryByTestId(mockBodyDataTestId);
+
+		expect(resumeWrapper?.children[1]).toBe(bodyWrapper);
+	});
+});
---
+++ src/views/Resume/Resume.tsx
@@ -0,0 +1,13 @@
+import { Header, Body } from '@layout';
+import './Resume.scss';
+
+function Resume() {
+	return (
+		<div className="resume">
+			<Header />
+			<Body />
+		</div>
+	);
+}
+
+export default Resume;
