977f9199f9e9e840cea3d38c7d4a3c08a31e207b
Ricky Davenport <ricky.davenport@wwt.com>
2022-03-27 21:21:22 -0500

    update moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -2,19 +2,12 @@ const { pathsToModuleNameMapper } = require('ts-jest');
 
 const { compilerOptions } = require('./tsconfig.json');
 
-const paths = pathsToModuleNameMapper(compilerOptions.paths);
-const tsPaths = Object.keys(paths).reduce((fullPaths, currentPathKey) => {
-	return {
-		...fullPaths,
-		[currentPathKey]: `<rootDir>/src/${paths[currentPathKey]}`
-	};
-}, {});
-
 module.exports = {
 	testEnvironment: 'jsdom',
-	modulePaths: [compilerOptions.baseUrl],
+	roots: ['<rootDir>/src'],
+	modulePaths: ['<rootDir>/src'],
 	moduleNameMapper: {
-		...tsPaths,
+		...pathsToModuleNameMapper(compilerOptions.paths),
 		'^.+\\.(css|scss)$': 'babel-jest'
 	},
 	setupFilesAfterEnv: [`<rootDir>/src/setupTests.ts`]
